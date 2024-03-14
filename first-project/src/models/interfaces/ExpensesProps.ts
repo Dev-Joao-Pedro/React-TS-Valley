305694a2c4499666e553b5fa5add134f8c1032f0
Joao-Pedro-Marques <marquesjoaopedro1234@gmail.com>
2024-03-14 19:22:57 -0300

    first project finalized

---
+++ first-project/src/models/interfaces/ExpensesProps.ts
@@ -0,0 +1,8 @@
+import { Movement } from "./Movements";
+
+export interface ExpensesProps {
+    emitMovement: (movement: Movement) => void
+    currentExpenses: number
+    currentBalance: number
+    
+}
